bc0524eb8271ae7bade1115184bd41127cedd165
nasrulilham <91175018+nasrulilham@users.noreply.github.com>
2023-04-17 02:32:00 +0700

    menambahkan API mahasiswa

--- TP_mod9_1302210041/TP_mod9_1302210041/Mahasiswa.cs
+++ TP_mod9_1302210041/TP_mod9_1302210041/Mahasiswa.cs
@@ -2,5 +2,13 @@
 {
     public class Mahasiswa
     {
+        public string nama { get; set; }
+        public string nim { get; set; }
+
+        public Mahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
     }
 }
---
+++ TP_mod9_1302210041/TP_mod9_1302210041/MahasiswaController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_mod9_1302210041
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswas = new List<Mahasiswa>
+        {
+            new Mahasiswa("Muhamad Ilham Nasrullah", "1302210041"),
+            new Mahasiswa("Adam Rafif Faqih", "1302210008"),
+            new Mahasiswa("Habli Zulvana Ath-Thaariq", "1302210024"),
+            new Mahasiswa("Maximus Bay", "1302210101"),
+            new Mahasiswa("Muhammad Isa Al Anshori", "1302210014")
+        };
+        //membuat get di controller
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswas;
+        }
+
+        //menambahkan object 
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mahasiswas.Add(value);
+        }
+
+        //get sesuia index
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mahasiswas[id];
+        }
+
+        //untuk mengahapus index
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mahasiswas.RemoveAt(id);
+        }
+    }
+}
